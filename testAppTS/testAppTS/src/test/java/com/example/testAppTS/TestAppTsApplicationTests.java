b3583f57d451eedbfab0a21d1469963643f77eda
Diego Araujo Olalla <araujo00627@gmail.com>
2023-05-11 22:01:13 -0500

    t

---
+++ testAppTS/testAppTS/src/test/java/com/example/testAppTS/TestAppTsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testAppTS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestAppTsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
